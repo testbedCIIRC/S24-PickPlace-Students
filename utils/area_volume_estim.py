7f35a2ec63be7f1cc9fa76d134f315051afb32a3
Miroslav David <mirasira54123698745@gmail.com>
2024-05-03 11:30:39 +0200

    fixed area and volume segmentation

--- utils/area_volume_estim.py
+++ utils/area_volume_estim.py
@@ -1,18 +1,16 @@
-"""Script used for estimating the area and volume of the waste detected on the conveyor.
-Calculation used for determinig the pixel size:
-
-47 cm - width of the conveyor
-561 pixels - width of the conveyor
-1 pixel = 0.0837 cm
-1 cm = 11.94 pixels
-1 pixel = 0.837 mm
+"""
+Script used for estimating the area and volume of the waste detected on the conveyor.
 """
 
 import numpy as np
 import cv2
 import torch
+import matplotlib.pyplot as plt
+import time
 
 PixelSize = 0.837 # mm
+Multiplier = 1.06e-3
+Multiplier = 0.0010969167011494897
 
 def GetInfoFromSegmentation(results, DepthImage, BackgroundPath):
 	""" 
@@ -34,18 +32,37 @@ def GetInfoFromSegmentation(results, DepthImage, BackgroundPath):
 	Background = npy_data[:, :, 3][100:600, 100:1200]
 	DepthImage = np.array(DepthImage, dtype=np.float32)
 	Background = np.array(Background, dtype=np.float32)
+	# print(f"{np.mean(Background)=}")
 	Difference = Background-DepthImage
-
+	
+	
 	for result in results:
 		ImageClasses = result.boxes.cls.cpu()
 		for i in range(int(torch.numel(ImageClasses))):
 			OutMask = np.array(cv2.resize(result.masks.data[i].cpu().numpy(), (1100, 500)))
+			HeighthMask = np.where(OutMask > 0.5, Difference, 0)
+			DepthMask = np.where(OutMask > 0.5, DepthImage, 0)
+			PixelAreas = (DepthMask*Multiplier)**2
+			Area = np.sum(PixelAreas)
+			# print(f"{Area=}")
+
+
 
 			pointsX, pointsY = np.where(OutMask > 0.5)
-			Areas.append(pointsX.size*PixelSize**2)
+			# print(pointsX.size)
+			Areas.append(Area)
 			Centroids.append([np.mean(pointsX), np.mean(pointsY)])
 
-			DepthMask = np.where(OutMask > 0.5, Difference, 0)
-			Volumes.append(np.sum(DepthMask)*PixelSize**2)
 			
+			Volumes.append(np.sum(PixelAreas*HeighthMask))
+			# plt.imshow(HeighthMask)
+			# plt.show()
+
+			# print(f"{np.sum(PixelAreas*HeighthMask)=}")
+			
+
+			# RealArea = 62370
+			# EstimatedMultiplier = np.sqrt(RealArea/np.sum(DepthMask**2))
+			# print(f"{EstimatedMultiplier=}")
+	# time.sleep(5)
 	return ImageClasses, Areas, Centroids, Volumes
